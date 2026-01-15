467dea91340f4b7c6e907f14c3283d712f482fab
Bhagyashri <bhagya.gadhave@gmail.com>
2026-01-15 12:43:35 +0530

    90. Step 11 - Create a simple hard coded currency exchange service

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.currency_exchange_service;
+import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class CurrencyExchangeController {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from, 
+			@PathVariable String to) {
+
+		// Hardcoded return for testing
+		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+	}
+}
