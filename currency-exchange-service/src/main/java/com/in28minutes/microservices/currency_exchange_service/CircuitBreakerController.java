a6d03ec661106cd6d5e104819ef45b686f810953
Bhagyashri <bhagya.gadhave@gmail.com>
2026-01-15 22:56:50 +0530

    add bulk-head feature

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -3,12 +3,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 @RestController
 public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @RateLimiter(name = "default")
+    @Bulkhead(name="sample-api")
     public String sampleApi() {
         logger.info("Sample api call received");
         return "sample-api";
