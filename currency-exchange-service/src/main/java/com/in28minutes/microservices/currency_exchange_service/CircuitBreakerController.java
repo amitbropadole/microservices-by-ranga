5b7400723a4fa88f9fed40a6577dff949af5de7c
Bhagyashri <bhagya.gadhave@gmail.com>
2026-01-15 22:49:16 +0530

    add fallback method for failed attempts

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -10,12 +10,15 @@ import io.github.resilience4j.retry.annotation.Retry;
 public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
         logger.info("Sample api call received");
         ResponseEntity<String> forEntity = new
                 RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
                 String.class);
         return forEntity.getBody();
+    }
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
     }
 }
