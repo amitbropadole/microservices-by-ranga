15f2afa85285523e0241849ecf4f4c3fc6754c88
Bhagyashri <bhagya.gadhave@gmail.com>
2026-01-15 22:45:25 +0530

    default retry 3 attempts of failed service

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -1,11 +1,21 @@
 package com.in28minutes.microservices.currency_exchange_service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import io.github.resilience4j.retry.annotation.Retry;
 @RestController
 public class CircuitBreakerController {
-
-    @GetMapping("sample-api")
-    public String sampleApi(){
-        return "Sample API";
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    @GetMapping("/sample-api")
+    @Retry(name = "default")
+    public String sampleApi() {
+        logger.info("Sample api call received");
+        ResponseEntity<String> forEntity = new
+                RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+                String.class);
+        return forEntity.getBody();
     }
 }
