1c6a7656a4a59fbc64b274d639cd43e0e4933ed1
Bhagyashri <bhagya.gadhave@gmail.com>
2026-01-15 19:44:50 +0530

    100. Step 18 - Using Feign REST Client for Service Invocation

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyConversionController.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.currency_conversion_service;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
+
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		// CALLING MICROSERVICE VIA FEIGN
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+		return new CurrencyConversion(
+				currencyConversion.getId(),
+				from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment() + " " + "feign"
+		);
+	}
 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
